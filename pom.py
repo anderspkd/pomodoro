e45b17c963ba5ede0cc633b776927b078044d534
Anders Dalskov <anderspkd@fastmail.com>
2018-11-29 23:29:01 +0100

    Update.

--- pom.py
+++ pom.py
@@ -1,104 +1,193 @@
 #!/usr/bin/env python3
 
+# Copyright: Anders Dalskov, 2018
+# See LICENSE
+
 import argparse
 import time
-import subprocess
-import os
-import json
-
-CONF_FILE = os.path.expanduser('~/.config/pom.conf')
-
-desc = ('Pomodoro technique utility.'
-        '\n\n'
-        'If run without any arguments, it uses settings present in config file'
-        '\n'
-        'from $HOME/.config/pom.conf.')
-
-p = argparse.ArgumentParser(description=desc, formatter_class=argparse.RawTextHelpFormatter)
-p.add_argument('-m', help='message to show in notifcation',
-               default='ding!', metavar='message')
-p.add_argument('-t', help='interval time', metavar='time', type=int)
-p.add_argument('-u', help='time unit', metavar='unit')
-p.add_argument('--notify-time', help='time notification is visible', default=None)
-p.add_argument('--config', help='config file')
-args = p.parse_args()
+import subprocess  # for send-notify
+import threading
+import re
+import shutil
 
 
-t = m = u = None
-msg = args.m
+description = """
+Pomodoro technique utility.
 
-def read_settings_from_config(conf):
-    global m
-    global t
-    global u
+The basic idea of the Pomodoro study technique is to work in intervals of e.g.,
+25 minutes. A short break is taken after each interval, and after a couple of
+intervals, a longer break is held.
 
-    if conf is None:
-        with open(CONF_FILE, 'r') as f:
-            x = json.loads(f.read())
-    elif not os.path.exists(conf):
-        print(f'No such file: {conf}')
-        exit(1)
+See: https://en.wikipedia.org/wiki/Pomodoro_Technique
+"""
+
+
+RES = re.compile(r'[0-9]+s')
+REM = re.compile(r'[0-9]+m')
+REH = re.compile(r'[0-9]+h')
+
+
+def parse_time(ts):
+    """Convert a string specifying a time interval into seconds.
+
+    Examples:
+
+    parse_time('25m')    =>   60 * 25 = 1500
+    parse_time('25m10s') =>   60 * 25 + 10 = 1510
+    parse_time('2h10s')  =>   3600 * 2 + 10 = 7210
+
+    The order of the units doesn't matter. I.e., '25m10s' and '10s25m' are
+    equivalent.
+
+    """
+    s = 0
+
+    # seconds
+    m = re.search(RES, ts)
+    if m is not None:
+        ss = m.group(0)
+        s = int(ss[:ss.index('s')])
+
+    # minutes
+    m = re.search(REM, ts)
+    if m is not None:
+        ss = m.group(0)
+        s += int(ss[:ss.index('m')]) * 60
+
+    # hours
+    m = re.search(REH, ts)
+    if m is not None:
+        ss = m.group(0)
+        s += int(ss[:ss.index('h')]) * 3600
+
+    # assume a time of 0 is because we could not parse the user's input.
+    if s == 0:
+        raise ValueError(f'could not parse string: {ts}')
     else:
-        with open(conf, 'r') as f:
-            x = json.loads(f.read())
-
-    t = int(x['time'])
-    u = x['unit']
-    if 'message' in x:
-        msg = x['message']
-    if u in ('s', 'seconds'):
-        m = 1
-        u = 'seconds'
-    elif u in ('m', 'minutes'):
-        m = 60
-        u = 'minutes'
-    elif u in ('h', 'hours'):
-        m = 60 * 60
-        u = 'hours'
-    if t == 1:
-        u = u[-1:]
+        return s
 
 
-if args.t is None:
-    read_settings_from_config(args.config)
+def format_time(tm):
+    """Converts an integer intepreted as seconds into a readable
+    representation. This is essentially the reverse of `parse_time`.
+
+    Examples:
+
+    format_time(1500)  =>  '25 minutes'
+    format_time(1510)  =>  '25 minutes 10 seconds'
+
+    """
+    tm = int(tm)
+
+    if tm <= 1:
+        return f'1 second!!'
+
+    # x minute(s) y second(s)
+    if 0 < tm < 3600:
+        tm_m = tm // 60
+        sm = f'{tm_m} minute{"" if tm_m == 1 else "s"}'
+        tm_s = tm % 60
+        ss = f'{tm_s} second{"" if tm_s == 1 else "s"}'
+        if tm_m > 0:
+            if tm_s > 0:
+                return sm + ' ' + ss
+            return sm
+        return ss
+
+    # x hour(s) y minute(s)
+    if 3600 <= tm:
+        tm_h = tm // 3600
+        sh = f'{tm_h} hour{"" if tm_h == 1 else "s"}'
+        tm_m = (tm % 3600) // 60
+        sm = f'{tm_m} minute{"" if tm_m == 1 else "s"}'
+        if tm_m > 0:
+            return sh + ' ' + sm
+        return sh
+
+
+def cli_input_loop(obj):
+    """A CLI based input loop.
+
+    `obj` is the object that the main thread (cf. the `run` function) uses to
+    pass data to this thread.
+
+    """
+
+    def show_remaining_time():
+        print(f'time left: {format_time(obj["remainingtime"])}')
+
+    print('[p] to pause, [q] to quit, [ENTER] to see remaining time')
+    show_remaining_time()
+
+    while True:
+        i = input()
+        if i == 'p':
+            show_remaining_time()
+            print('Press any key to unpause ...')
+            obj['paused'] = True
+            input()
+            obj['paused'] = False
+        if i == '':
+            show_remaining_time()
+        if i == 'q':
+            obj['bail'] = True
+            show_remaining_time()
+            print('exiting ...')
+            break
+
+
+if shutil.which('notify-send') is not None:
+    def notify_user_done(msg):
+        subprocess.run(['notify-send', msg])
 else:
-    t = args.t
-    if args.u not in ('m', 'minutes', 'h', 'hours'):
-        m = 1
-        u = 'seconds'
-    elif args.u in ('m', 'minutes'):
-        m = 60
-        u = 'minutes'
-    elif args.u in ('h', 'hours'):
-        m = 60 * 60
-        u = 'hours'
-    if t == 1:
-        u = u[-1:]
-
-t = t * m
-
-def notify(msg, t=None):
-    cmd = ['notify-send', f'{msg}']
-    if t:
-        cmd += ['-t', t]
-    subprocess.run(cmd)
+    def notify_user_done(msg):
+        print(msg)
 
 
-print(f'Interval is {t//m} {u}. Hit Ctrl-C to pause or stop.')
+def run(interval, msg):
+    """Main loop.
 
-stime = time.time()
-while True:
-    try:
-        time.sleep(t)
-        break
-    except KeyboardInterrupt:
-        print()
-        # remaining time
-        t = int(t - (time.time() - stime))
-        stime = time.time()
-        y = input('stop(q)?, or Enter to continue... ')
-        if y == 'q':
-            exit(0)
-        print(f'{t//m} {u} remaining ...')
+    Information about whether or not the user bailed, paused as well as
+    remaining time and so on, is stored in `obj`. This dict is used to
+    communicate with the thread handling user input.
 
-notify(args.m, t=args.notify_time)
+    """
+
+    # setup the "communication channel" with the other thread.
+    obj = {'remainingtime': parse_time(interval), 'paused': False,
+           'bail': False}
+
+    # start input thread
+    t = threading.Thread(target=cli_input_loop, args=(obj,), daemon=True)
+    t.start()
+
+    # we're done if `interval` time has elapsed or the user exited early.
+    while not (obj['remainingtime'] <= 0 or obj['bail'] is True):
+        time.sleep(1)
+        # this is a bit crude, but works well enough for our purposes.
+        obj['remainingtime'] -= 1
+        if obj['paused']:
+            # wait for user to unpause
+            while obj['paused']:
+                time.sleep(1)
+
+    # finally, signal the user if we exited normally. Otherwise just exit
+    # silently (we assume the user is aware of a premature exit, since this
+    # would have been performed by the user)
+    if not obj['bail']:
+        notify_user_done(msg)
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description=description,
+                                formatter_class=argparse.RawTextHelpFormatter)
+    p.add_argument('--message',
+                   help='notification message',
+                   default='ding!'
+    )
+    p.add_argument('--interval',
+                   help='interval time',
+                   default='25m'
+    )
+    a = p.parse_args()
+    run(a.interval, a.message)
